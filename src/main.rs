7eb04d4374a10a78a38a973c2ce8d05e2974d59b
James Cook <mokus@deepbondi.net>
2015-08-16 09:30:11 -0700

    ported to STM32L476 discovery board

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,67 @@
-#![feature(plugin, no_std, core, start, core_intrinsics)]
+#![feature(plugin, no_std, start, lang_items)]
 #![no_std]
-#![plugin(macro_zinc)]
 
-extern crate core;
-extern crate zinc;
+extern crate stm32;
+extern crate rlibc;
 
-use zinc::hal::timer::Timer;
-use zinc::hal::pin::Gpio;
-use zinc::hal::stm32f4::{pin, timer};
+use stm32::stm32l476::*;
 
-#[zinc_main]
-pub fn main() {
-  zinc::hal::mem_init::init_stack();
-  zinc::hal::mem_init::init_data();
+#[start]
+pub fn main(_: isize, _: *const *const u8) -> isize {
+    stm32::init_memory();
+    
+    // enable LED GPIOs (PB2 = red, PE8 = green)
+    RCC.ahb2enr.set_gpiob_en(true).set_gpioe_en(true);
+    
+    // set LED pins to "output"
+    GPIOB.moder.set_mode(2, stm32::gpio::GPIO_moder_mode::Output);
+    GPIOE.moder.set_mode(8, stm32::gpio::GPIO_moder_mode::Output);
+    
+    // enable TIM2
+    RCC.apb1enr1.set_tim2_en(true);
+    
+    // configure TIM2.  System clock at boot is 4 MHz and we haven't
+    // changed it, so setting prescaler to 3999 will yield a 1 ms tick.
+    // CEN bit switches the timer on.
+    TIM2.psc.set_psc(3999);
+    TIM2.cr1.set_cen(stm32::timer::GPTIM32_cr1_cen::Enable);
+    
+    // apply configuration changes to TIM2
+    TIM2.egr.set_ug(true);
+    
+    loop {
+        // Red on, Green off
+        GPIOB.bsrr.set_bs(2, true);
+        GPIOE.bsrr.set_br(8, true);
+        wait_ms(300);
+        
+        // Green on, Red off
+        GPIOB.bsrr.set_br(2, true);
+        GPIOE.bsrr.set_bs(8, true);
+        wait_ms(300);
+    }
+}
 
-  let led1 = pin::Pin {
-    port: pin::Port::PortD,
-    pin: 13u8,
-    function: pin::Function::GPIOOut
-  };
-  let led2 = pin::Pin {
-    port: pin::Port::PortD,
-    pin: 14u8,
-    function: pin::Function::GPIOOut
-  };
-  led1.setup();
-  led2.setup();
+fn wait_ms(ms : u32) {
+    let start = TIM2.cnt.cnt();
+    while TIM2.cnt.cnt() - start < ms
+    {
+        // just spin
+    }
+}
 
-  let timer = timer::Timer::new(timer::TimerPeripheral::Timer2, 25u32);
+#[lang="panic_fmt"]
+pub fn panic_fmt(_: &core::fmt::Arguments, _: &(&'static str, usize)) -> ! {
+    loop {}
+}
 
-  loop {
-    led1.set_high();
-    led2.set_low();
-    timer.wait_ms(300);
-    led1.set_low();
-    led2.set_high();
-    timer.wait_ms(300);
-  }
+#[lang="stack_exhausted"]
+extern fn stack_exhausted() {}
+
+#[lang="eh_personality"]
+extern fn eh_personality() {}
+
+#[no_mangle]
+pub extern fn abort() -> ! {
+  loop {}
 }
