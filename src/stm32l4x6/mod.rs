8427e731f266a654b1978e5d7cc71d656e6a4c7c
James Cook <mokus@deepbondi.net>
2015-08-24 19:51:34 -0700

    mapped out all currently-supported stm32l4 registers

--- src/stm32l4x6/mod.rs
+++ src/stm32l4x6/mod.rs
@@ -12,14 +12,16 @@ pub mod pwr;
 pub mod rcc;
 pub mod timer_opt;
 
+use crc;
 use gpio;
+use rng;
+use rtc;
 use timer;
 
 extern {
+    #[link_name="stm32l4x6_CRC"]        pub static CRC:         crc::CRC;
     #[link_name="stm32l4x6_FIREWALL"]   pub static FIREWALL:    firewall::FW;
-    
     #[link_name="stm32l4x6_FLASH"]      pub static FLASH:       flash::FLASH;
-    
     #[link_name="stm32l4x6_GPIOA"]      pub static GPIOA:       gpio::GPIO;
     #[link_name="stm32l4x6_GPIOB"]      pub static GPIOB:       gpio::GPIO;
     #[link_name="stm32l4x6_GPIOC"]      pub static GPIOC:       gpio::GPIO;
@@ -28,11 +30,10 @@ extern {
     #[link_name="stm32l4x6_GPIOF"]      pub static GPIOF:       gpio::GPIO;
     #[link_name="stm32l4x6_GPIOG"]      pub static GPIOG:       gpio::GPIO;
     #[link_name="stm32l4x6_GPIOH"]      pub static GPIOH:       gpio::GPIO;
-    
     #[link_name="stm32l4x6_PWR"]        pub static PWR:         pwr::PWR;
-    
     #[link_name="stm32l4x6_RCC"]        pub static RCC:         rcc::RCC;
-    
+    #[link_name="stm32l4x6_RNG"]        pub static RNG:         rng::RNG;
+    #[link_name="stm32l4x6_RTC"]        pub static RTC:         rtc::RTC;
     #[link_name="stm32l4x6_TIM2"]       pub static TIM2:        timer::GPTIM32;
     #[link_name="stm32l4x6_TIM2_OR"]    pub static TIM2_OR:     timer_opt::TIM2_OPT;
     #[link_name="stm32l4x6_TIM3"]       pub static TIM3:        timer::GPTIM32;
