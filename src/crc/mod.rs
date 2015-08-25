3b507de2097bbfb10bc88628a05c97e9d60b3a8c
James Cook <mokus@deepbondi.net>
2015-08-24 17:21:22 -0700

    Checking CRC layout in a couple more reference manuals

--- src/crc/mod.rs
+++ src/crc/mod.rs
@@ -3,7 +3,26 @@
 use volatile_cell::VolatileCell;
 
 // Known to apply to:
-//  STM32L4x6
+//  [RM0090] STM32F4
+ioregs!(CRC_BASIC = {
+    0x00 => reg32 dr {
+        0..31 => dr : rw,
+    },
+    
+    0x04 => reg32 idr {
+        0..7 => idr : rw,
+    },
+    
+    0x08 => reg32 cr {
+        0 => reset : wo {
+            1 => Reset,
+        },
+    },
+});
+
+// Known to apply to:
+//  [RM0091] STM32F0x1, STM32F0x2, STM32F0x8
+//  [RM0351] STM32L4x6
 ioregs!(CRC = {
     0x00 => reg32 dr {
         0..31 => dr : rw,
@@ -15,7 +34,6 @@ ioregs!(CRC = {
     
     0x08 => reg32 cr {
         0 => reset : wo {
-            0 => Dummy, // rustc --explain E0083
             1 => Reset,
         },
         3..4 => polysize : rw {
